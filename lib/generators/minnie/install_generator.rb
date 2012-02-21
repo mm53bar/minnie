cf439fcc733934d48ac26e0421ccca557bb6fb55
Michael McClenaghan <mike@sideline.ca>
2012-02-21 12:34:19 -0700

    Added the beginnings of an install generator

---
+++ lib/generators/minnie/install_generator.rb
@@ -0,0 +1,19 @@
+module Minnie
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+
+      source_root File.expand_path('../templates', __FILE__)
+      desc "Configures your app with the basics for Minnie to execute."
+
+      def copy_locale
+        copy_file "en.yml", "config/locales/minnie.en.yml"
+      end
+
+      def add_session_routes
+        route "resources :sessions, :only => [:new, :create]"
+        route "match '/signin' => 'sessions#new', :as => :signin"
+        route "match '/signout' => 'sessions#destroy', :as => :signout"
+      end
+    end
+  end
+end
