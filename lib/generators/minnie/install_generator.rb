2f6141126eb3d3086676f82645e98e8f891ac42b
Michael McClenaghan <mike@sideline.ca>
2012-02-29 13:36:16 -0700

    Updating path for user::auth

--- lib/generators/minnie/install_generator.rb
+++ lib/generators/minnie/install_generator.rb
@@ -34,7 +34,8 @@ module Minnie
           generate("model", "user email:string password_digest:string")
         end
         inject_into_class 'app/models/user.rb', User do
-          "  include Minnie::User::Auth\n\n  has_secure_password\n\n  attr_accessible :email, :password\n  validates_presence_of :email\n  validates_presence_of :password, :on => :create\n\n"
+          "hi"
+          #"  include Minnie::User::Auth\n\n  has_secure_password\n\n  attr_accessible :email, :password\n  validates_presence_of :email\n  validates_presence_of :password, :on => :create\n\n"
         end
       end
     end
--- lib/minnie/user/auth.rb
+++ lib/minnie/user/auth.rb
@@ -1,11 +1,13 @@
 module Minnie
-  module Auth
-    extend ActiveSupport::Concern    
+  module User
+    module Auth
+      extend ActiveSupport::Concern    
 
-    module ClassMethods
-      def authenticate(email, password)
-        user = User.first(conditions: {email: email})
-        return user && user.authenticate(password)        
+      module ClassMethods
+        def authenticate(email, password)
+          user = User.first(conditions: {email: email})
+          return user && user.authenticate(password)        
+        end
       end
     end
   end
